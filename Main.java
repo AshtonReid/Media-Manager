c403cccfbef0b7342357e5bcc04e5c144029af7e
AshtonReid <152633523+AshtonReid@users.noreply.github.com>
2023-12-07 15:12:50 +0000

    UI Init

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
-public class Main {
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    new Main(); // Runs the GUI
+  }
+  public Main() {
+    setSize(600, 400);
+    setLayout(null);
+    Container c = getContentPane();
+    c.setBackground(new Color(0, 0, 0, 5));
+    setVisible(true);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
   }
 }
