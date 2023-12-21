5378474160677fb7ba525a60bf8bb7a09d19f1ae
AGR <152633523+AshtonReid@users.noreply.github.com>
2023-12-20 22:40:50 -0500

    big commit

---
+++ src/main/java/com/mediamanager/MainApplication.java
@@ -0,0 +1,31 @@
+// MainApplication.java
+package com.mediamanager;
+
+import com.mediamanager.controllers.YourController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+		// Load FXML file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
+        Parent root = loader.load();
+
+        // Set the controller
+        YourController controller = loader.getController();
+
+        Scene scene = new Scene(root, 1024, 640);
+        primaryStage.setTitle("Media Manager");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/mediamanager/controllers/YourController.java
@@ -0,0 +1,13 @@
+// YourController.java
+package com.mediamanager.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class YourController {
+    
+	@FXML
+    void settingsBtnClick(MouseEvent event) {
+		System.out.println("Clicked");
+    }
+}
