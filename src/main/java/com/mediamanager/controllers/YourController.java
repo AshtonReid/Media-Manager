a25332e1683dd4f7c85a2733f53e8bc71b3289e9
AGR <152633523+AshtonReid@users.noreply.github.com>
2023-12-21 22:11:03 -0500

    little nav

--- src/main/java/com/mediamanager/controllers/YourController.java
+++ src/main/java/com/mediamanager/controllers/YourController.java
@@ -1,13 +1,42 @@
 // YourController.java
 package com.mediamanager.controllers;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class YourController {
-    
+	
 	@FXML
     void settingsBtnClick(MouseEvent event) {
-		System.out.println("Clicked");
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Settings.fxml"));
+			Parent root = loader.load();
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root));
+			stage.setTitle("Settings");
+			stage.show();
+		} catch (IOException err) {
+			err.printStackTrace();
+		}
     }
+
+	@FXML
+	void infoBtnClick(MouseEvent event) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Info.fxml"));
+			Parent root = loader.load();
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root));
+			stage.setTitle("Info");
+			stage.show();
+		} catch (IOException err) {
+			err.printStackTrace();
+		}
+	}
 }
