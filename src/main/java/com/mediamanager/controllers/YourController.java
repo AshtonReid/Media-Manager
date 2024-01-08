4558c7d67642bf0ee55cf40af1831e066693cf90
AshtonReid <152633523+AshtonReid@users.noreply.github.com>
2024-01-08 08:19:49 -0500

    Commit

--- src/main/java/com/mediamanager/controllers/YourController.java
+++ src/main/java/com/mediamanager/controllers/YourController.java
@@ -2,6 +2,10 @@
 package com.mediamanager.controllers;
 
 import java.io.IOException;
+import javafx.animation.Interpolator;
+
+import javafx.animation.RotateTransition;
+import javafx.util.Duration;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,34 +13,47 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.scene.image.ImageView;
+
 
 public class YourController {
-	
-	@FXML
+    
+    @FXML
+    private ImageView settingsImg;
+
+    @FXML
     void settingsBtnClick(MouseEvent event) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Settings.fxml"));
-			Parent root = loader.load();
-			Stage stage = new Stage();
-			stage.setScene(new Scene(root));
-			stage.setTitle("Settings");
-			stage.show();
-		} catch (IOException err) {
-			err.printStackTrace();
-		}
+        try {
+            
+//            RotateTransition rotateTransition = new RotateTransition(Duration.seconds(1), settingsImg);
+//            rotateTransition.setFromAngle(0);
+//            rotateTransition.setToAngle(360);
+//            
+//            rotateTransition.setInterpolator(Interpolator.EASE_IN);
+//            rotateTransition.play();
+            
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Settings.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Settings");
+            stage.show();
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
     }
 
-	@FXML
-	void infoBtnClick(MouseEvent event) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Info.fxml"));
-			Parent root = loader.load();
-			Stage stage = new Stage();
-			stage.setScene(new Scene(root));
-			stage.setTitle("Info");
-			stage.show();
-		} catch (IOException err) {
-			err.printStackTrace();
-		}
-	}
+    @FXML
+    void infoBtnClick(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Info.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Info");
+            stage.show();
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+    }
 }
