0d375b6bff5482ebe73ebd416f64ccc1447572ca
AGR <152633523+AshtonReid@users.noreply.github.com>
2024-01-18 23:43:07 -0500

    b

--- src/main/java/com/mediamanager/controllers/YourController.java
+++ src/main/java/com/mediamanager/controllers/YourController.java
@@ -18,71 +18,77 @@ import javafx.scene.control.Button;
 import com.mediamanager.SpotifyAPI;
 
 public class YourController {
-    
-    private Stage stage;
-    private Stage settingsStage;
-    private Stage infoStage;
 
-    @FXML
-    private ImageView settingsImg;
+	private Stage stage;
+	private Stage settingsStage;
+	private Stage infoStage;
 
-    @FXML
-    private Text genreTitle;
-    
-    @FXML
-    private Button readMoreBtn;
+	@FXML
+	private ImageView settingsImg;
 
-    @FXML
-    void settingsBtnClick(MouseEvent event) {
-        if (settingsStage == null || !settingsStage.isShowing()) {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Settings.fxml"));
-                Parent root = loader.load();
-                settingsStage = new Stage();
-                settingsStage.setScene(new Scene(root));
-                settingsStage.setTitle("Settings");
-                settingsStage.show();
-            } catch (IOException err) {
-                err.printStackTrace();
-            }
-        }
-    }
+	@FXML
+	private Text genreTitle;
 
-    @FXML
-    void infoBtnClick(MouseEvent event) {
-        if (infoStage == null || !infoStage.isShowing()) {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Info.fxml"));
-                Parent root = loader.load();
-//                infoStage = new Stage();
-//                infoStage.setScene(new Scene(root));
-//                infoStage.setTitle("Info");
-//                infoStage.show();
-                this.stage.setScene(new Scene(root));
-                this.stage.show();
-            } catch (IOException err) {
-                err.printStackTrace();
-            }
-        }
-    }
-    
-    @FXML
-    void readMoreBtnClick(MouseEvent event) {
-        System.out.println("no");
-    }
-    
-    public void setStage(Stage stage) {
-        this.stage = stage;
-    }
+	@FXML
+	private Button readMoreBtn;
 
-    public void initialize() {
-        Random random = new Random();
-        String[] genres = SpotifyAPI.getGenres();
-        int randomIndex = random.nextInt(genres.length);
+	@FXML
+	void settingsBtnClick(MouseEvent event) {
+		if (settingsStage == null || !settingsStage.isShowing()) {
+			try {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Settings.fxml"));
+				Parent root = loader.load();
+				settingsStage = new Stage();
+				settingsStage.setScene(new Scene(root));
+				settingsStage.setTitle("Settings");
+				settingsStage.show();
+			} catch (IOException err) {
+				err.printStackTrace();
+			}
+		}
+	}
 
-        genreTitle.setText(genres[randomIndex].toUpperCase());
+	@FXML
+	void infoBtnClick(MouseEvent event) {
+		if (infoStage == null || !infoStage.isShowing()) {
+			try {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Info.fxml"));
+				Parent root = loader.load();
+				infoStage = new Stage();
+				infoStage.setScene(new Scene(root));
+				infoStage.setTitle("Info");
+				infoStage.show();
 
-        Font.loadFont(getClass().getResourceAsStream("/fonts/Knewave.ttf"), 60);
-        genreTitle.setStyle("-fx-font-family: 'Knewave'; -fx-font-size: 69;");
-    }
+			} catch (IOException err) {
+				err.printStackTrace();
+			}
+		}
+	}
+
+	@FXML
+	void readMoreBtnClick(MouseEvent event) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MusicPlayer.fxml"));
+			Parent root = loader.load();
+			this.stage.setScene(new Scene(root));
+			this.stage.show();
+		} catch (IOException err) {
+			err.printStackTrace();
+		}
+	}
+
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
+
+	public void initialize() {
+		Random random = new Random();
+		String[] genres = SpotifyAPI.getGenres();
+		int randomIndex = random.nextInt(genres.length);
+
+		genreTitle.setText(genres[randomIndex].toUpperCase());
+
+		Font.loadFont(getClass().getResourceAsStream("/fonts/Knewave.ttf"), 60);
+		genreTitle.setStyle("-fx-font-family: 'Knewave'; -fx-font-size: 69;");
+	}
 }
