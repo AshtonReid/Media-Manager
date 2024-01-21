d7b47c153fdf86d062137cb1befec31952b8b04f
AshtonReid <152633523+AshtonReid@users.noreply.github.com>
2024-01-20 20:04:53 -0500

    Commit

--- src/main/java/com/mediamanager/controllers/YourController.java
+++ src/main/java/com/mediamanager/controllers/YourController.java
@@ -10,6 +10,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
@@ -19,76 +20,72 @@ import com.mediamanager.SpotifyAPI;
 
 public class YourController {
 
-	private Stage stage;
-	private Stage settingsStage;
-	private Stage infoStage;
+    private Stage settingsStage;
+    private Stage infoStage;
 
-	@FXML
-	private ImageView settingsImg;
+    @FXML
+    private ImageView settingsImg;
 
-	@FXML
-	private Text genreTitle;
+    @FXML
+    private Text genreTitle;
 
-	@FXML
-	private Button readMoreBtn;
+    @FXML
+    private Button readMoreBtn;
 
-	@FXML
-	void settingsBtnClick(MouseEvent event) {
-		if (settingsStage == null || !settingsStage.isShowing()) {
-			try {
-				FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Settings.fxml"));
-				Parent root = loader.load();
-				settingsStage = new Stage();
-				settingsStage.setScene(new Scene(root));
-				settingsStage.setTitle("Settings");
-				settingsStage.show();
-			} catch (IOException err) {
-				err.printStackTrace();
-			}
-		}
-	}
+    @FXML
+    void settingsBtnClick(MouseEvent event) {
+        if (settingsStage == null || !settingsStage.isShowing()) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Settings.fxml"));
+                Parent root = loader.load();
+                settingsStage = new Stage();
+                settingsStage.setScene(new Scene(root));
+                settingsStage.setTitle("Settings");
+                settingsStage.show();
+            } catch (IOException err) {
+                err.printStackTrace();
+            }
+        }
+    }
 
-	@FXML
-	void infoBtnClick(MouseEvent event) {
-		if (infoStage == null || !infoStage.isShowing()) {
-			try {
-				FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Info.fxml"));
-				Parent root = loader.load();
-				infoStage = new Stage();
-				infoStage.setScene(new Scene(root));
-				infoStage.setTitle("Info");
-				infoStage.show();
+    @FXML
+    void infoBtnClick(MouseEvent event) {
+        if (infoStage == null || !infoStage.isShowing()) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Info.fxml"));
+                Parent root = loader.load();
+                infoStage = new Stage();
+                infoStage.setScene(new Scene(root));
+                infoStage.setTitle("Info");
+                infoStage.show();
 
-			} catch (IOException err) {
-				err.printStackTrace();
-			}
-		}
-	}
+            } catch (IOException err) {
+                err.printStackTrace();
+            }
+        }
+    }
 
-	@FXML
-	void readMoreBtnClick(MouseEvent event) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MusicPlayer.fxml"));
-			Parent root = loader.load();
-			this.stage.setScene(new Scene(root));
-			this.stage.show();
-		} catch (IOException err) {
-			err.printStackTrace();
-		}
-	}
+    @FXML
+    void readMoreBtnClick(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MusicPlayer.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+    }
 
-	public void setStage(Stage stage) {
-		this.stage = stage;
-	}
+    public void initialize() {
+        Random random = new Random();
+        String[] genres = SpotifyAPI.getGenres();
+        int randomIndex = random.nextInt(genres.length);
 
-	public void initialize() {
-		Random random = new Random();
-		String[] genres = SpotifyAPI.getGenres();
-		int randomIndex = random.nextInt(genres.length);
+        genreTitle.setText(genres[randomIndex].toUpperCase());
 
-		genreTitle.setText(genres[randomIndex].toUpperCase());
-
-		Font.loadFont(getClass().getResourceAsStream("/fonts/Knewave.ttf"), 60);
-		genreTitle.setStyle("-fx-font-family: 'Knewave'; -fx-font-size: 69;");
-	}
+        Font.loadFont(getClass().getResourceAsStream("/fonts/Knewave.ttf"), 60);
+        genreTitle.setStyle("-fx-font-family: 'Knewave'; -fx-font-size: 69;");
+    }
 }
